85641cfc7e9aa14fb629fd107ea2567313addbf5
Michal Greszta <michalgreszta@Michals-MacBook-Air.local>
2023-07-30 01:35:48 +0200

    the idea was wrong, tory byly zle, podwozie bylo zle. switched to a new implementation of authorization. now using .env (shocker).

---
+++ dataFetch.py
@@ -0,0 +1,17 @@
+import requests
+import spotipy
+import os
+from dotenv import load_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
+load_dotenv()
+SPOTIPY_CLIENT_ID=os.getenv('CLIENT_ID')
+SPOTIPY_CLIENT_SECRET=os.getenv('CLIENT_SECRET')
+SPOTIPY_REDIRECT_URI='http://127.0.0.1:9090'
+SCOPE = 'user-top-read'
+
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
+
+results = sp.current_user_top_tracks()
+
+print(results)
