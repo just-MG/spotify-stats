efb9caaca827cbea9aa142bdba33a6b377fbfdbb
Michal Greszta <michalgreszta@Michals-MacBook-Air.local>
2023-07-30 12:25:17 +0200

    updated files

--- dataFetch.py
+++ dataFetch.py
@@ -7,7 +7,7 @@ from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 SPOTIPY_CLIENT_ID=os.getenv('CLIENT_ID')
 SPOTIPY_CLIENT_SECRET=os.getenv('CLIENT_SECRET')
-SPOTIPY_REDIRECT_URI='http://127.0.0.1:9090'
+SPOTIPY_REDIRECT_URI='http://127.0.0.1'
 SCOPE = 'user-top-read'
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIPY_CLIENT_ID, client_secret=SPOTIPY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
